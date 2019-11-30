724391e6b35a42a84725416843e36b7d3795aa2d
Felix Ding <felixding1989@gmail.com>
2019-11-29 17:11:45 -0800

    implement operable first stage scrape

--- scraper/index.js
+++ scraper/index.js
@@ -3,9 +3,9 @@ const Nightmare = require('nightmare');
 const nightmare = Nightmare({show: true, pollInterval: 50});
 
 const siteUrl = "https://www.airbnb.com/s/Sunnyvale--CA--United-States/homes?adults=0&children=0&infants=0&place_id=ChIJO13QqUW2j4ARosN83Sb7jXY&refinement_paths%5B%5D=%2Fhomes&search_type=section_navigation";
-const className = '._1jl4bye9';
-const imgClassName = '._1i2fr3fi';
-
+const individualRoomClickableDOM = '._ttw0d';
+const nextPageDOM = '._r4n1gzb';
+const totalNumberOfPagesDOM = '._1bdke5s';
 // fetch the page
 const getResults = async () => {
   return nightmare
@@ -15,19 +15,41 @@ const getResults = async () => {
       value: '636',
       path: '/query',
       secure: true
-    }).wait(imgClassName).evaluate((selector1, selector2) => {
-      var roomList = document.querySelectorAll(selector1);
-      var imageList = document.querySelectorAll(selector2);
-      var tempArray = [];
-      for (var i = 0; i < roomList.length; i++) {
-        tempArray.push(roomList[i].innerText + '\n' + imageList[i].style.backgroundImage.substring(5, imageList[i].style.backgroundImage.length - 2));
-      }
-      return String(tempArray);
-    }, className, imgClassName).end().then((result) => {
-      return result;
-    }).catch((err) => {
-      console.log(err);
-    });
+    }).wait(individualRoomClickableDOM, totalNumberOfPagesDOM, nextPageDOM)
+      .evaluate((totalNumberOfPagesDOM) => {
+        var pageArray = document.querySelectorAll(totalNumberOfPagesDOM);
+        var totalNumberOfPages = Number(pageArray[pageArray.length - 1].innerText);
+        return totalNumberOfPages;
+      }, totalNumberOfPagesDOM)
+      .then((result) => {
+        var helperArray = new Array(result).fill(nextPageDOM);
+        var linkToNextPage = siteUrl;
+        var linkBin = [];
+        var lastPage = result;
+        return helperArray.reduce((accumulator, toGo, index) => {
+          return accumulator.then((results) => {
+            return nightmare
+              .goto(linkToNextPage)
+              .wait(individualRoomClickableDOM, totalNumberOfPagesDOM)
+              .evaluate((individualRoomClickableDOM, lastPage, toGo) => {
+                var currentPageDOMList = Array.from(document.querySelectorAll(individualRoomClickableDOM));
+                var currentPagehrefList = currentPageDOMList.map((element) => {
+                  return element.children[0].href;
+                });
+                var currentPage = Number(document.querySelector('._e602arm').innerText);
+                var nextPage = currentPage < lastPage ? document.querySelector(toGo).children[0].href : null;
+                return {currentPagehrefList, nextPage};
+              }, individualRoomClickableDOM, lastPage, toGo)
+              .then(({currentPagehrefList, nextPage}) => {
+                linkToNextPage = nextPage;
+                linkBin = linkBin.concat(currentPagehrefList);
+                return linkBin;
+              });
+          });
+        }, Promise.resolve([])).then((result) => {
+          debugger;
+        });
+      });
 }
 
 // return the organized data
--- server/router.js
+++ server/router.js
@@ -6,6 +6,8 @@ router.get('/scrape', async (req, res, next) => {
   const results = await getResults();
   // res.render('index', results);
   console.log(results);
+  // gather info scraped and write it into a file locally
+  
   console.log('get router has been used...');
 });
 
